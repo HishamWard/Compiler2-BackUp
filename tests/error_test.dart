53ab39dd100be2481f4d8274e2902e38bdbf1bb5
RedWn <redwaniscool@gmail.com>
2023-06-19 22:05:11 +0300

    All error tests in one file
    makes jack a jacky boy

--- tests/error_test.dart
+++ tests/error_test.dart
@@ -1,9 +1,59 @@
-class test {
-  void func() {
-    String x = "Hi";
-  }
+// class test {
+//   void func() {
+//     String x = "Hi";
+//   }
+// }
+//
+//
+// class test {
+//   void func2() {
+//     String x = "Hi";
+//   }
+// }
 
-  void func() {
-    String x = "Hi, again";
-  }
-}
+
+
+// class test {
+//   void func() {
+//     String x = "Hi";
+//   }
+//
+//
+//   void func() {
+//     String x = "Hi, again";
+//   }
+// }
+
+
+
+// class test {
+//   void func() {
+//     x = "Hi";
+//   }
+// }
+//
+//
+//
+// class test {
+//   double x;
+//
+//
+//   void func() {
+//     x = "Hi";
+//   }
+// }
+//
+//
+//
+// var a = 5;
+// a = "Hi";
+//
+//
+//
+// double a = 5;
+// double a = 6;
+//
+//
+//
+// double a = 5;
+// String b = a;
